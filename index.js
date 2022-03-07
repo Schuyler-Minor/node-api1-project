30f4d412e16173e426c8d80050853c1890d34897
Schuyler <skym1n0r91@gmail.com>
2022-03-07 17:25:00 -0600

    starting

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const server = require('./api/server');
+// const server = require("./api/server");
 
-const port = 9000;
+// const port = 9000;
 
 // START YOUR SERVER HERE
+
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.end("Hello you beautiful people");
+});
+
+console.log("qwerty");
+server.listen(9000, () => {
+  console.log("server is running");
+});
