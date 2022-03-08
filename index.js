6972556199f69c36e29bf38b884b229806eebf11
Schuyler <skym1n0r91@gmail.com>
2022-03-07 18:49:16 -0600

    delete

--- index.js
+++ index.js
@@ -6,13 +6,31 @@
 
 const express = require("express");
 
+const Users = require("./api/users/model");
+
 const server = express();
 
-server.get("/", (req, res) => {
-  res.end("Hello you beautiful people");
+server.get("/json", (req, res) => {
+  res.json({ a: 1, b: 2, c: 3 });
 });
 
-console.log("qwerty");
+server.post("/json", (req, res) => {
+  console.log("received POST!");
+  res.end();
+});
+
+server.get("/api/users", (req, res) => {
+  Users.find().then((users) => {
+    res.json(users);
+  });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  Users.remove(req.params.id).then((user) => {
+    res.json(user);
+  });
+});
+
 server.listen(9000, () => {
   console.log("server is running");
 });
